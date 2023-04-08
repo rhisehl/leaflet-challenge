9a66828879d1959cb315c94836e6ccd323d2ffe9
Rhi Sehl <rhisehl@gmail.com>
2023-04-08 10:19:44 -0600

    Completed file

--- Part1_Earthquake_Visualization/static/js/logic.js
+++ Part1_Earthquake_Visualization/static/js/logic.js
@@ -1,55 +1,69 @@
 // import URL for GeoJSON
 const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson"
 
-// Populate all functions into the data promise
-d3.json(url).then(function (data){
-    gatherData(data.features)
+// determine the color for the markers based on depth of the earthquake
+function chooseColor(depth){
+    if(depth <10 ) return "green";
+    else if (depth < 30) return "lightgreen";
+    else if (depth < 50) return "yellow";
+    else if (depth < 70) return "orange";
+    else if (depth < 90) return "salmon";
+    else return "red";
+}
+
+// initialize the map
+var myMap = L.map("map", {
+    center: [
+                    37.09, -95.71,
+                ],
+                zoom: 4,
+  });
+// add the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+// data promise
+d3.json(url).then(function(data){
+    // print data in console in case there is an issue
+    console.log(data);
+    // add the data to a layer based on the geoJSON file
+    L.geoJSON(data,{
+        // create new circle markers for each point
+        pointToLayer: function(feature, latlng){
+            return new L.CircleMarker(latlng,{
+                // radius is based on the magnitude
+                radius: 2*(feature.properties.mag),
+                // color is based on the chooseColor function
+                color:chooseColor(feature.geometry.coordinates[2]),
+                // set fill opacity to 1 to eliminate the borders
+                fillOpacity: 1
+            })
+        },
+        // bind a popup with additional information about the earthquake
+        onEachFeature: function(feature,layer){
+            layer.bindPopup("<h3>"+ feature.properties.place + "</h3><hr><p> Magnitude: " + 
+            feature.properties.mag + '&nbsp; &nbsp; &nbsp;' + "Depth: " + feature.geometry.coordinates[2] + "</br>" + (new Date (feature.properties.time)));
+        }
+    }).addTo(myMap);
+
+    // set up legend on the map
+    var legend = L.control({position: "bottomright"});
+    // create the legend
+    legend.onAdd = function(myMap){
+        var div = L.DomUtil.create("div","info legend");
+        limits = [-10, 10, 30, 50, 70,90];
+        labels = [];
+        // add the colors and information to an array for the legend    
+        for (var i=0; i<limits.length; i++){
+            labels.push(
+            '<li style=\"background-color:' + chooseColor(limits[i] +1) + '"></li>' +
+            " " + limits[i] + (limits[i+1]? '&ndash;' + limits[i+1] + '<br>' : '+'));
+        }
+        // push all the labels to the innerHTML of the legend
+        div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+        return div;
+    };
+    // add legend to map
+    legend.addTo(myMap)
 });
-
-
-function gatherData (eqData) {
-    // loop through features to add popups with place, time, and magnitude of earthquake
-    function featureLoop(feature, layer){
-        layer.bindPopup('<h3>${feature.properties.place}</h3><hr><p> Magnitude: ${feature.properties.mag} </br>${new Dat(feature.properties.time)}');
-    }
-    // add geoJSON layer with all features in eqData
-    // run featureLoop on each data point
-    var earthquakes = L.geoJSON(eqData, {
-        featureLoop: featureLoop
-    });
-    // run the mapInit function
-    mapInit(earthquakes);
-}
-
-function mapInit(earthquakes) {
-    // base layers
-    var Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-    });
-    var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-	maxZoom: 17,
-	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-    });
-
-    // baseMaps object
-    var baseMaps = {
-        "Street Map": Stadia_OSMBright,
-        "Topographic Map": OpenTopoMap
-    }
-    // Create Overlay
-    var overlayMaps = {
-        "Earthquakes" : earthquakes
-    }
-    // Create Map
-    var earthquakeMap = L.map("map",{
-        center: [
-            37.09, -95.71
-        ],
-        layers: [Stadia_OSMBright,earthquakes]
-    });
-    // Create Layer Controls
-    L.control.layers(baseMaps, overlayMaps, {
-        collapsed: false
-    }).addTo(earthquakeMap);
-}
